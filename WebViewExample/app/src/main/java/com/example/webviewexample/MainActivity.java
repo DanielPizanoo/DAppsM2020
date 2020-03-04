a6cc8313f8f11d82282fa0c5d1f23ecc94b996aa
DanielPizanoo <danielhp74@gmail.com>
2020-03-04 01:00:20 -0600

    p

--- WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
+++ WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         btnswitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
