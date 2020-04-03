2decd479d5b53f77573d2173dcd0b08791b07f8e
DanielPizanoo <danielhp74@gmail.com>
2020-04-02 18:08:45 -0600

    MiFragmento

---
+++ MiBitacora/app/src/main/java/com/example/mibitacora/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.mibitacora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txt;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Relacionamos componentes
+        txt = (EditText) findViewById(R.id.txtCampo);
+        btn = (Button) findViewById(R.id.btnGuardar);
+        String bitacoraCompleta = "";
+
+        //Rutina para leer el archivo bitacora
+        String [] archivos = fileList(); //Lee los nombres de los archivos de texto de la app
+
+        try {
+            if (fileExiste(archivos, "bitacora.txt")) {
+                InputStreamReader archivo = new InputStreamReader(openFileInput("bitacora.txt"));
+                BufferedReader br = new BufferedReader(archivo);
+                //Verficiar si el archivo tiene datos
+                String linea = br.readLine();
+
+                while (linea != null) {
+                    bitacoraCompleta = bitacoraCompleta + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+
+                //Copiamos el contenido del archivo bitacora.txt al campo de texto multilinea
+                txt.setText(bitacoraCompleta);
+            }
+        } catch (IOException ex) {
+
+        }
+    }
+
+    //Funcion para validar si existe el archivo bitacora.txt dentro de la lista de archivos creados por la app
+    private boolean fileExiste(String [] archivos, String nameFile) {
+        for (int i = 0; i < archivos.length; i++)
+            if (nameFile.equals(archivos[i]))
+                return true;
+        return false;
+    }
+
+    //Metodo para guardar la bitacora tecleada por el usuario en el campo de texto en un archivo bitacora.txt
+    public void Guardar(View vista) {
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("bitacora.txt", Activity.MODE_PRIVATE));
+            archivo.write(txt.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException closed) {
+
+        }
+
+        Toast.makeText(this, "Archivo guardado correctamente", Toast.LENGTH_SHORT).show();
+        //Cerrar aplicacion
+        finish();
+
+    }
+}
